52b1bd18c7480ea2c46136638b8f10482f745b5b
Mathana Nair Sreedaran <mathana96@gmail.com>
2017-04-19 14:58:18 +0100

    huffman working I think

--- huffkov.py
+++ huffkov.py
@@ -1,5 +1,24 @@
+# http://www.techrepublic.com/article/huffman-coding-in-python/
+
 import collections
-# mydict = dict((k, v if v else '') for k, v in mydict.items())
+from heapq import heappush, heappop, heapify
+from collections import defaultdict
+ 
+def encode(symb2freq):
+    """Huffman encode the given dict mapping symbols to weights"""
+    heap = [[wt, [sym, ""]] for sym, wt in symb2freq.items()]
+    heapify(heap)
+    while len(heap) > 1:
+        lo = heappop(heap)
+        hi = heappop(heap)
+        for pair in lo[1:]:
+            pair[1] = '0' + pair[1]
+        for pair in hi[1:]:
+            pair[1] = '1' + pair[1]
+        heappush(heap, [lo[0] + hi[0]] + lo[1:] + hi[1:])
+    return sorted(heappop(heap)[1:], key=lambda p: (len(p[-1]), p))
+ 
+
 
 def distribution(input):
     freq = collections.Counter(input)
@@ -7,7 +26,6 @@ def distribution(input):
     relfreq = dict((k, v/n) for k, v in freq.items())
     print (freq)
     return relfreq
-
 
 
 source = open("input.txt")
@@ -20,3 +38,9 @@ print('Length of text message (in bits): ', 7*len(input))
 # Construct probabilities using observed frequencies in given string of text
 rv = distribution(input)
 print(rv)
+
+huff = encode(rv)
+
+print(huff)
+
+
