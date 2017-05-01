78b74b74f36a649485adbc41bc990fccee16e25f
Mathana Nair Sreedaran <mathana96@gmail.com>
2017-05-01 17:43:11 +0100

    Matrix populated

--- markov.py
+++ markov.py
@@ -3,121 +3,44 @@ import collections
 from heapq import heappush, heappop, heapify
 from collections import defaultdict
 
-# def markov():
-    
-# def huffman_markov:
 
+matrix = np.zeros((128, 128))
+matrix_dict = {}
 
-def encode(symb2freq):
-    heap = [[wt, [sym, ""]] for sym, wt in symb2freq.items()]
-    heapify(heap)
-    while len(heap) > 1:
-        lo = heappop(heap)
-        hi = heappop(heap)
-        for pair in lo[1:]:
-            pair[1] = '0' + pair[1]
-        for pair in hi[1:]:
-            pair[1] = '1' + pair[1]
-        heappush(heap, [lo[0] + hi[0]] + lo[1:] + hi[1:])
-    return sorted(heappop(heap)[1:], key=lambda p: (len(p[-1]), p))
+def distribution(input):
+    freq = collections.Counter(input)
+    n = float(len(input))
+    relfreq = dict((k, v/n) for k, v in freq.items())
+    return relfreq
 
-def encode_decode(dictionary, text):
-    res = ""
-    while text:
-        for k in dictionary:
-            if text.startswith(k):
-                res += dictionary[k]
-                text = text[len(k):]
-    return(res)
+def buildMatrixDict(rvd):
+    m = {}
+    j = 0
+    for key, value in rvd.iteritems():
+        m[key] = j    
+        j += 1
+    return m
+        
+def populateMatrix():
+    length = len(input)
+    for i in range(0, length):
+        current_char = input[i]
+        if i != (length-1):
+            next_char = input[i + 1]
+            current_no = matrix_dict[current_char]
+            next_no = matrix_dict[next_char]
+            matrix[current_no, next_no] += 1
+            
 
-def huffman_code_first_dict(huffman):
-	data = []
-	code = []
-	for item in huffman:
-		data.append(item[0])
-		code.append(item[1])
-
-	return dict(zip(code,data))
-
-def huffman_data_first_dict(huffman):
-	data = []
-	code = []
-	for item in huffman:
-		data.append(item[0])
-		code.append(item[1])
-
-	return dict(zip(data,code))
-
-def entropy(data):
-    cleanData=[x for x in data if x!=0]
-    sumData=sum(cleanData)
-    probs=np.array(cleanData)/sumData
-    return(-sum(probs*np.log2(probs)))
-
-def codewordlength(rv, huffman):
-	cwlList =[]
-	for k, v in rv.items():
-		for code in huffman:
-			bits = code[1]
-			bitlength = len(bits)
-			if (code[0] == k):
-				cwlList.append(v*bitlength)
-	return sum(cwlList)
-
-
+            
 source = open("input.txt")
 input = source.read()
+rv = distribution(input)
 
-print('Markov chain representation ...')
+matrix_dict = buildMatrixDict(rv)
 
-# markov()
+print(matrix_dict)
+populateMatrix()
 
-print('Entropy of Markov chain (in bits) = ', entropy())
-
-# huffman_markov()
-
-# print('Huffman L(C) for Markov chain (in bits) = ', codewordlength(?,?))
-
-print('Test 1 - Small sample (with no unique sequence pair,  eg 'u' is only char following 'q') ...')
-sample = input[0:30]
-
-print('Length of source message (in bits) = ', len(sample) * 7)
-
-# encoded_markov
-
-print('Length of encoded message (in bits) = ', len(encoded_markov))
-
-# decoded_markov
-
-if (len(sample) < 50):
-	print(sample)
-	print(encoded_markov)
-	print(decoded_markov)
-
-
-if (sample == decoded_markov):
-	print('YES !! decoded(endoded(message)) == message')
-else:
-	print('ERROR !! decoded(endoded(message)) != message')
-	length = len(decoded_markov)
-	for i in range(0, length):
-		print(sample[k], decoded_markov[k])
-
-print('Test 1 - Small sample (with no unique sequence pair,  eg 'u' is only char following 'q') ...')
-sample = input
-
-print('Length of source message (in bits) = ', len(sample) * 7)
-
-# encoded_markov
-
-print('Length of encoded message (in bits) = ', len(encoded_markov))
-
-# decoded_markov
-
-if (sample == decoded_markov):
-	print('YES !! decoded(endoded(message)) == message')
-else:
-	print('ERROR !! decoded(endoded(message)) != message')
-	length = len(decoded_markov)
-	for i in range(0, length):
-		print(sample[k], decoded_markov[k])
+# print(matrix[47, :])
+# print(matrix)
